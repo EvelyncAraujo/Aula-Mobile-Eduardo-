1995706a8d408f224721543f8303aa099f3fceea
evelyncaraujo <evyaraujoifc@gmail.com>
2024-02-27 11:02:15 -0300

    listagem-cmt 01

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import ListagemProdutos from '@/components/ListagemProdutos.vue';
+
 </script>
 
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <span>Em breve</span>
+  <ListagemProdutos />
 </template>
+
 
 <style scoped>
 header {
